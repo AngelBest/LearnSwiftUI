0cb6cc0b4b1d88730460fb248179e28f9a14ced8
sxt <sxtsyf111@163.com>
2023-06-25 16:33:17 +0800

    test

---
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by 宋秀腾 on 2023-06-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MapView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDemo/SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by 宋秀腾 on 2023-06-25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
